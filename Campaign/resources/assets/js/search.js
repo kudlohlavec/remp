3f588cca58251bfc8d74933407c1a82868caf4da
Milan Martinkovic <martinkovic.milan91@gmail.com>
2020-03-04 16:23:38 +0100

    search - dynamic search display populating

--- Campaign/resources/assets/js/search.js
+++ Campaign/resources/assets/js/search.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
     $.typeahead({
         input: ".js-typeahead",
-        order: "asc",
         dynamic: true,
         debug: true,
         filter: false,
-        display: ["name", "banners"],
         group: "type",
         source: {
-            groupName: {
-                ajax: {
-                    url: '/search',
-                    data: {
-                        term: '{{query}}'
-                    },
-                    // path: 'banner'
-                }
+            ajax: {
+                url: '/search',
+                data: {
+                    term: '{{query}}'
+                },
             }
         },
+        //uncomment if custom type-related template will be needed
+        /*template: function (query, item) {
+            if (item.type === 'campaign') {
+                return '{{name}}, related banners:{{banners}}'
+            }
+
+            return '{{name}}';
+        },*/
         callback: {
-            onInit: function () {
-                console.log('typeahead init');
+            onPopulateSource: function (node, data, group, path) {
+                let displayKeys = new Set();
+
+                data.forEach( searchResult => {
+                    //get relevant search keys from current searchResult
+                    const searchKeys = Object.keys(searchResult).filter(isSearchRelevantKey);
+                    //add search keys to displayKeys set (add method adds only unique items into the set)
+                    searchKeys.forEach(searchKey => displayKeys.add(searchKey));
+                });
+
+                this.options.display = Array.from(displayKeys);
+                return data;
             },
             onClickBefore: function (node, a, item, event) {
                 event.preventDefault();
@@ -29,4 +42,10 @@ $(document).ready(function() {
         }
     });
 });
+
+function isSearchRelevantKey(key) {
+    const irelevantKeys = ['type', 'search_result_url', 'group'];
+
+    return !irelevantKeys.includes(key);
+}
 
